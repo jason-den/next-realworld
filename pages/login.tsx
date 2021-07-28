1c7f10751823c9ebad60ca1467d10a94dd8d0144
Jason Den <jason.den.cloud@gmail.com>
2021-07-28 12:06:40 +1000

    improve login

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,15 +9,37 @@ import Link from 'next/link';
 import Router from 'next/router';
 import { Auth } from 'lib/api';
 import { useState } from 'react';
-
 import styles from 'styles/Login.module.css';
 
 export default function SignIn() {
+  return (
+    <>
+      <Head>
+        <title>Login | Next Realworld</title>
+        <meta name="description" content="Welcome back" />
+      </Head>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link href="/register" as="/register">
+                <a className={styles.link}>Need an account?</a>
+              </Link>
+            </p>
+            <SignInForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+function SignInForm() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
   const onSubmit = async () => {
-    const { user, status, error } = await Auth.login(email, password);
+    const { error } = await Auth.login(email, password);
     if (error) {
       alert(error);
     } else {
@@ -27,44 +49,30 @@ export default function SignIn() {
 
   return (
     <>
-      <Head>
-        <title>Login | Next Realworld</title>
-        <meta name="description" content="Welcome back" />
-      </Head>
-      <div className="container page">
-        <h1 className="text-xs-center">Sign in</h1>
-        <div className="w-100 text-center">
-          <Link href="/register" as="/register">
-            <a className={styles.link}>Need an account?</a>
-          </Link>
-        </div>
-
-        <div className="mt-2 mt-lg-2">
-          <input
-            className="form-control form-control-lg"
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mt-2 mt-lg-2">
-          <input
-            className="form-control form-control-lg"
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className={styles.item}>
-          <button className="my-2 btn btn-lg btn-primary pull-xs-right" onClick={(e) => onSubmit()}>
-            Sign in
-          </button>
-        </div>
+      <div className="form-group">
+        <input
+          className="form-control form-control-lg"
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
+      <div className="form-group">
+        <input
+          className="form-control form-control-lg"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button className="my-2 btn btn-lg btn-primary pull-xs-right" onClick={(e) => onSubmit()}>
+        Sign in
+      </button>
     </>
   );
 }
