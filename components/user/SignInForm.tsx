9ad48a744f16ad3384dfd2e52be450e8fcb8984a
Jason Den <jason.den.cloud@gmail.com>
2021-07-28 12:12:47 +1000

    improve Register; Refactor: move component out

---
+++ components/user/SignInForm.tsx
@@ -0,0 +1,44 @@
+import Router from 'next/router';
+import { Auth } from 'lib/api';
+import { useState } from 'react';
+
+export function SignInForm() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onSubmit = async () => {
+    const { error } = await Auth.login(email, password);
+    if (error) {
+      alert(error);
+    } else {
+      Router.push('/');
+    }
+  };
+
+  return (
+    <>
+      <div className="form-group">
+        <input
+          className="form-control form-control-lg"
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control form-control-lg"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} />
+      </div>
+
+      <button className="my-2 btn btn-lg btn-primary pull-xs-right" onClick={(e) => onSubmit()}>
+        Sign in
+      </button>
+    </>
+  );
+}
--- pages/register.tsx
+++ pages/register.tsx
@@ -6,27 +6,11 @@ Sign in/Sign up pages (URL: /#/login, /#/register )
 */
 import Head from 'next/head';
 import Link from 'next/link';
-import Router from 'next/router';
-import { Auth } from 'lib/api';
-import { useState } from 'react';
 
 import styles from 'styles/Login.module.css';
+import { RegisterForm } from '../components/user/RegisterForm';
 
 export default function Page() {
-  const [userName, setUserName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  const onSubmit = async () => {
-    const { user, errors } = await Auth.signup(userName, email, password);
-    if (errors) {
-      console.log(errors);
-    } else {
-      console.log('successfully registered', { user });
-      Router.push('/');
-    }
-  };
-
   return (
     <>
       <Head>
@@ -34,47 +18,17 @@ export default function Page() {
         <meta name="description" content="Welcome!" />
       </Head>
       <div className="container page">
-        <h1 className="text-xs-center">Sign up</h1>
-        <div className="w-100 text-center">
-          <Link href="/register" as="/register">
-            <a className={styles.link}>Have an account?</a>
-          </Link>
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign up</h1>
+            <div className="w-100 text-center">
+              <Link href="/register" as="/register">
+                <a className={styles.link}>Have an account?</a>
+              </Link>
+            </div>
+          </div>
         </div>
-
-        <div className="mt-2 mt-lg-2">
-          <input
-            className="form-control form-control-lg"
-            name="username"
-            type="text"
-            placeholder="User Name"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </div>
-        <div className="mt-2 mt-lg-2">
-          <input
-            className="form-control form-control-lg"
-            name="email"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mt-2 mt-lg-2">
-          <input
-            className="form-control form-control-lg"
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <button className="my-2 btn btn-lg btn-primary pull-xs-right" onClick={(e) => onSubmit()}>
-          Register
-        </button>
+        <RegisterForm />
       </div>
     </>
   );
