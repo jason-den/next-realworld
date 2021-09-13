46e848bedf2207dd9da0fb8aabd4cc0fa967068b
Jason Den <jason.den.cloud@gmail.com>
2021-09-13 10:12:02 +1000

    bug fix - footer covering main content

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 export const Footer = () => (
-  <footer style={{ position: 'relative' }}>
+  <footer>
     <div className="container">
       <Link href="/" passHref>
         <a className="logo-font" children="conduit" />
       </Link>
       <span className="attribution">
-        An interactive learning project from <a>Thinkster</a>. Code &amp; design licensed under MIT.
+        An interactive learning project from <a>Thinkster</a>. Code &amp; design licensed under MIT. Check out the{' '}
+        <a href="https://github.com/jason-den/next-realworld">implementation with Next.js and TypeScript</a> .
       </span>
     </div>
   </footer>
--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -3,11 +3,11 @@ import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <>
+  <div style={{ paddingBottom: '40px' }}>
     <Navbar />
     {children}
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
