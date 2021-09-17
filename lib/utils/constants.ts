f648f5884487306fbdfd5f99f4667e8a38153d47
Jason Den <jason.den.cloud@gmail.com>
2021-09-17 13:53:16 +1000

    implement article/slug page

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = `https://conduit.productionready.io/api`;
+// export const BASE_URL = 'https://realworld-temp-api.herokuapp.com/api';
 export const DEFAULT_AVATAR_URL = 'https://static.productionready.io/images/smiley-cyrus.jpg';
